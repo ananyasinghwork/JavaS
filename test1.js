4e61c02d774ab3014523af1a04b4195432ae1c79
ananyasinghworks <ananyasingh0421@gmail.com>
2023-08-05 11:35:13 +0530

    Operations on Variables

--- test1.js
+++ test1.js
@@ -56,3 +56,44 @@ console.log(product);
 var quotient = 66/33;
 console.log(quotient);
 
+//increment numbers
+var myVar=87;
+myVar= myVar+1;
+console.log(myVar);
+
+//easier way to increment numbers
+var myVari=87;
+myVari++;
+console.log(myVari);
+
+//decrement
+var myVaria=89;
+myVaria--;
+console.log(myVaria);
+
+//decimal numbers
+var ourDecimal=5.7;
+var myDecimal=0.009;
+
+//multiply decimal numbers
+var product1= 2.0 * 2.5;
+console.log(product1);
+
+//dividing decimal numbers
+var quotient1= 5.5/5.0;
+console.log(quotient1);
+
+//remainder operation
+var remainder;
+remainder = 11%3;
+console.log(remainder);
+
+//augmented math operations
+var a=2;
+var b=4;
+var c=17;
+
+a+=5;
+b-=2;
+c*=10;
+console.log(a,b,c);
