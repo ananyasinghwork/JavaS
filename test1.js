32777c9e6dc6216f5d2e118fd95e8a2a6286ddb9
ananyasinghworks <ananyasingh0421@gmail.com>
2023-08-05 11:05:05 +0530

    Variable Play

---
+++ test1.js
@@ -0,0 +1,24 @@
+var number=5;// in-line comment
+
+/* this is a
+multi-line comment */
+
+number=9;
+
+/* the datatypes are:
+undefined,null,number,boolean,string,symbol and object */
+
+// Declaring Variables
+//var can be used throughout the program
+var myName="Ananya"
+myName=8
+
+//let is limited to scope
+let ourName="Singh"
+
+//cannot be changed 
+const pi=3.14
+
+/* declaration and assignment of variables*/
+var a;
+var b=2;
