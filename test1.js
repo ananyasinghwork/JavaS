a4fbf4427849a7d4ab0de04b2dc98a36cf1be6f5
ananyasinghworks <ananyasingh0421@gmail.com>
2023-08-05 11:20:18 +0530

    Operations added

--- test1.js
+++ test1.js
@@ -1,4 +1,4 @@
-var number=5;// in-line comment
+var number=5; // in-line comment
 
 /* this is a
 multi-line comment */
@@ -22,3 +22,24 @@ const pi=3.14
 /* declaration and assignment of variables*/
 var a;
 var b=2;
+console.log(a);
+
+//console use
+a=7;
+b=a;
+console.log(a);
+
+//intitalised variables
+var a=5;
+var b=10;
+var c="This is a ";
+
+//uninitialised variables
+a=a+1;
+b=b+1
+
+//it is case-sensitive
+
+//adding numbers
+var a=10+10;
+console.log(a);
